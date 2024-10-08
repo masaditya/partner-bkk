21740890575b222043548ba66fad6cf0ed56c3b3
triard <triard78@gmail.com>
2024-10-09 06:56:58 +0700

    add tem dashboard

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>
+    Dashboard | BKK SMK 1 Bojonegoro
+</title>
+<link rel="icon" href="favicon.ico">
+<link href="{{ asset('tail-admin/style.css') }}" rel="stylesheet">
---
+++ resources/views/includes/js.blade.php
@@ -0,0 +1,1 @@
+<script defer src="{{ asset('tail-admin/bundle.js') }}"></script>
