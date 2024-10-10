74139b37f82b4bd875699f2cc7e462b2dac97a9c
triard <triard78@gmail.com>
2024-10-11 03:38:27 +0700

    add modal logout and avatar profile

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -29,7 +29,8 @@
             </button>
             <!-- Hamburger Toggle BTN -->
             <a class="block flex-shrink-0 lg:hidden" href="index.html">
-                <img class="object-cover w-8 h-8" src="{{ asset('images/LOGO-BKK-SMKN-1-BOJONEGORO.jpg') }}" alt="Logo BKK SMKN 1 Bojonegoro" />
+                <img class="object-cover w-8 h-8" src="{{ asset('images/LOGO-BKK-SMKN-1-BOJONEGORO.jpg') }}"
+                    alt="Logo BKK SMKN 1 Bojonegoro" />
             </a>
         </div>
         <div class="hidden sm:block">
@@ -175,13 +176,23 @@
             <!-- User Area -->
             <div class="relative" x-data="{ dropdownOpen: false }" @click.outside="dropdownOpen = false">
                 <a class="flex items-center gap-4" href="#" @click.prevent="dropdownOpen = ! dropdownOpen">
+
+                    @php
+                    $user = Auth::user();
+                    $name = $user->name;
+                    $initials = collect(explode(' ', $name))->map(function($word) {
+                    return strtoupper(substr($word, 0, 1));
+                    })->join('');
+                    @endphp
+
                     <span class="hidden text-right lg:block">
-                        <span class="block text-sm font-medium text-black dark:text-white">Wijaya Saputra</span>
+                        <span class="block text-sm font-medium text-black dark:text-white">{{ $name }}</span>
                         <span class="block text-xs font-medium">Admin</span>
                     </span>
 
-                    <span class="h-12 w-12 rounded-full">
-                        <img src="{{ asset('tail-admin/src/images/user/user-01.png') }}" alt="User" />
+                    <span
+                        class="h-12 w-12 rounded-full flex items-center justify-center bg-gray-500 text-white font-bold">
+                        {{ $initials }}
                     </span>
 
                     <svg :class="dropdownOpen && 'rotate-180'" class="hidden fill-current sm:block" width="12"
@@ -212,23 +223,22 @@
                             </a>
                         </li>
                     </ul>
-                    <form action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <button
-                            class="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
-                            type="submit">
-                            <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M15.5375 0.618744H11.6531C10.7594 0.618744 10.0031 1.37499 10.0031 2.26874V4.64062C10.0031 5.05312 10.3469 5.39687 10.7594 5.39687C11.1719 5.39687 11.55 5.05312 11.55 4.64062V2.23437C11.55 2.16562 11.5844 2.13124 11.6531 2.13124H15.5375C16.3625 2.13124 17.0156 2.78437 17.0156 3.60937V18.3562C17.0156 19.1812 16.3625 19.8344 15.5375 19.8344H11.6531C11.5844 19.8344 11.55 19.8 11.55 19.7312V17.3594C11.55 16.9469 11.2062 16.6031 10.7594 16.6031C10.3125 16.6031 10.0031 16.9469 10.0031 17.3594V19.7312C10.0031 20.625 10.7594 21.3812 11.6531 21.3812H15.5375C17.2219 21.3812 18.5625 20.0062 18.5625 18.3562V3.64374C18.5625 1.95937 17.1875 0.618744 15.5375 0.618744Z"
-                                    fill="" />
-                                <path
-                                    d="M6.05001 11.7563H12.2031C12.6156 11.7563 12.9594 11.4125 12.9594 11C12.9594 10.5875 12.6156 10.2438 12.2031 10.2438H6.08439L8.21564 8.07813C8.52501 7.76875 8.52501 7.2875 8.21564 6.97812C7.90626 6.66875 7.42501 6.66875 7.11564 6.97812L3.67814 10.4844C3.36876 10.7938 3.36876 11.275 3.67814 11.5844L7.11564 15.0906C7.25314 15.2281 7.45939 15.3312 7.66564 15.3312C7.87189 15.3312 8.04376 15.2625 8.21564 15.125C8.52501 14.8156 8.52501 14.3344 8.21564 14.025L6.05001 11.7563Z"
-                                    fill="" />
-                            </svg>
-                            Keluar
-                        </button>
-                    </form>
+
+                    <button
+                        class="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base"
+                        type="button" aria-haspopup="dialog" aria-expanded="false" aria-controls="hs-sign-out-alert"
+                        data-hs-overlay="#hs-sign-out-alert">
+                        <svg class="fill-current" width="22" height="22" viewBox="0 0 22 22" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M15.5375 0.618744H11.6531C10.7594 0.618744 10.0031 1.37499 10.0031 2.26874V4.64062C10.0031 5.05312 10.3469 5.39687 10.7594 5.39687C11.1719 5.39687 11.55 5.05312 11.55 4.64062V2.23437C11.55 2.16562 11.5844 2.13124 11.6531 2.13124H15.5375C16.3625 2.13124 17.0156 2.78437 17.0156 3.60937V18.3562C17.0156 19.1812 16.3625 19.8344 15.5375 19.8344H11.6531C11.5844 19.8344 11.55 19.8 11.55 19.7312V17.3594C11.55 16.9469 11.2062 16.6031 10.7594 16.6031C10.3125 16.6031 10.0031 16.9469 10.0031 17.3594V19.7312C10.0031 20.625 10.7594 21.3812 11.6531 21.3812H15.5375C17.2219 21.3812 18.5625 20.0062 18.5625 18.3562V3.64374C18.5625 1.95937 17.1875 0.618744 15.5375 0.618744Z"
+                                fill="" />
+                            <path
+                                d="M6.05001 11.7563H12.2031C12.6156 11.7563 12.9594 11.4125 12.9594 11C12.9594 10.5875 12.6156 10.2438 12.2031 10.2438H6.08439L8.21564 8.07813C8.52501 7.76875 8.52501 7.2875 8.21564 6.97812C7.90626 6.66875 7.42501 6.66875 7.11564 6.97812L3.67814 10.4844C3.36876 10.7938 3.36876 11.275 3.67814 11.5844L7.11564 15.0906C7.25314 15.2281 7.45939 15.3312 7.66564 15.3312C7.87189 15.3312 8.04376 15.2625 8.21564 15.125C8.52501 14.8156 8.52501 14.3344 8.21564 14.025L6.05001 11.7563Z"
+                                fill="" />
+                        </svg>
+                        Keluar
+                    </button>
                 </div>
                 <!-- Dropdown End -->
             </div>
@@ -236,3 +246,61 @@
         </div>
     </div>
 </header>
+
+
+<div id="hs-sign-out-alert"
+    class="hs-overlay hidden size-full fixed top-0 start-0 overflow-x-hidden overflow-y-auto z-[5000]" role="dialog"
+    tabindex="-1" aria-labelledby="hs-sign-out-alert-label">
+    <div
+        class="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-xs sm:w-full m-3 sm:mx-auto">
+        <div class="relative flex flex-col bg-white shadow-lg rounded-xl dark:bg-neutral-900">
+            <div class="absolute top-2 end-2">
+                <button type="button"
+                    class="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600"
+                    aria-label="Close" data-hs-overlay="#hs-sign-out-alert">
+                    <span class="sr-only">Close</span>
+                    <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path d="M18 6 6 18" />
+                        <path d="m6 6 12 12" /></svg>
+                </button>
+            </div>
+
+            <div class="p-4 sm:p-10 text-center overflow-y-auto">
+                <!-- Icon -->
+                <span
+                    class="mb-4 inline-flex justify-center items-center size-[62px] rounded-full border-4 border-yellow-50 bg-yellow-100 text-yellow-500 dark:bg-yellow-700 dark:border-yellow-600 dark:text-yellow-100">
+                    <svg class="shrink-0 size-5" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                        fill="currentColor" viewBox="0 0 16 16">
+                        <path
+                            d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                    </svg>
+                </span>
+                <!-- End Icon -->
+
+                <h3 id="hs-sign-out-alert-label" class="mb-2 text-2xl font-bold text-gray-800 dark:text-neutral-200">
+                    Keluar
+                </h3>
+                <p class="text-gray-500 dark:text-neutral-500">
+                    Apakah Anda yakin ingin keluar dari akun BKK SIGMA Anda?
+                </p>
+
+                <div class="mt-6 grid gap-y-2">
+                    <form action="{{ route('logout') }}" method="POST">
+                        @csrf
+                        <button type="submit"
+                            class="py-2.5 px-4 w-full inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none focus:outline-none focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
+                            Keluar
+                        </button>
+                    </form>
+                    <button type="button"
+                        class="py-2.5 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-meta-3 text-white hover:bg-emerald-700 focus:outline-none focus:bg-emerald-700 disabled:opacity-50 disabled:pointer-events-none"
+                        data-hs-overlay="#hs-sign-out-alert">
+                        Batal
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
